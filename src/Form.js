7d4081d1a86bfec2db33642edddeab7ed7633517
Alexey <>
2021-09-09 22:19:26 +0500

    Работа

---
+++ src/Form.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from "react";
+import {
+	NameField,
+	EmailField,
+	PhoneField,
+	LanguageField,
+	CheckField,
+} from "./Inputs";
+
+const Form = () => {
+	const [isFormValid, setIsFormValid] = useState(false);
+	const [validInputs, setValidInputs] = useState({
+		name: false,
+		email: false,
+		phone: false,
+		language: false,
+		check: false,
+	});
+	const validate = (value, type) => {
+		switch (type) {
+			case "name":
+				if (/^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/.test(value))
+					setValidInputs({ ...validInputs, [type]: true });
+				else setValidInputs({ ...validInputs, [type]: false });
+				break;
+			case "email":
+				if (/\S+@\S+\.\S+/.test(value))
+					setValidInputs({ ...validInputs, [type]: true });
+				else setValidInputs({ ...validInputs, [type]: false });
+				break;
+			case "phone":
+				if (/^((\+7|7|8)+([0-9]){10})$/.test(value))
+					setValidInputs({ ...validInputs, [type]: true });
+				else setValidInputs({ ...validInputs, [type]: false });
+				break;
+			case "language":
+				if (["Русский", "Английский", "Китайский", "Испанский"].includes(value))
+					setValidInputs({ ...validInputs, [type]: true });
+				else setValidInputs({ ...validInputs, [type]: false });
+				break;
+			case "check":
+				if (value === true) setValidInputs({ ...validInputs, [type]: true });
+				else setValidInputs({ ...validInputs, [type]: false });
+				break;
+			default:
+				console.log("Возникла ошибка при валидации");
+		}
+	};
+
+	useEffect(() => {
+		if (Object.values(validInputs).every((x) => x === true))
+			setIsFormValid(true);
+		else setIsFormValid(false);
+	}, [validInputs]);
+
+	return (
+		<div className="App__form">
+			<h2>Регистрация</h2>
+			<span>
+				Уже есть аккаунт? <a href="#">Войти</a>
+			</span>
+			<NameField validate={validate} isValid={validInputs.name}/>
+			<EmailField validate={validate} isValid={validInputs.email}/>
+			<PhoneField validate={validate} isValid={validInputs.phone}/>
+			<LanguageField validate={validate} isValid={validInputs.language}/>
+			<CheckField validate={validate} isValid={validInputs.check}/>
+			<button className="button" disabled={!isFormValid}>
+				Зарегистрироваться
+			</button>
+		</div>
+	);
+};
+
+
+export default Form;
---
+++ src/Inputs.js
@@ -0,0 +1,140 @@
+import React, { useState } from "react";
+
+export const NameField = ({ validate, isValid }) => {
+	const placeholderValue = "Введите Ваше имя";
+	const [value, setValue] = useState("");
+	const handleChange = (event) => {
+		setValue(event.target.value);
+		validate(event.target.value, "name");
+	};
+
+	return (
+		<div className="form__text-field">
+			<label className="text-field__label">Имя</label>
+			<input
+				className=""
+				placeholder={placeholderValue}
+				onChange={handleChange}
+				value={value}
+			/>
+			{!isValid && (
+				<label className="text-field__error-message error-message">
+					Введите корректное значение
+				</label>
+			)}
+		</div>
+	);
+};
+
+export const EmailField = ({ validate, isValid }) => {
+	const placeholderValue = "Введите Ваш email";
+	const [value, setValue] = useState("");
+	const handleChange = (event) => {
+		setValue(event.target.value);
+		validate(event.target.value, "email");
+	};
+	return (
+		<div className="form__text-field">
+			<label className="text-field__label">Email</label>
+			<input
+				className=""
+				placeholder={placeholderValue}
+				onChange={handleChange}
+				value={value}
+			/>
+			{!isValid && (
+				<label className="text-field__error-message error-message">
+					Введите корректное значение
+				</label>
+			)}
+		</div>
+	);
+};
+
+export const PhoneField = ({ validate, isValid }) => {
+	const placeholderValue = "Введите номер телефона";
+	const [value, setValue] = useState("");
+	const handleChange = (event) => {
+		setValue(event.target.value);
+		validate(event.target.value, "phone");
+	};
+	return (
+		<div className="form__text-field">
+			<label className="text-field__label">Номер телефона</label>
+			<input
+				className=""
+				placeholder={placeholderValue}
+				onChange={handleChange}
+				value={value}
+			/>
+			{!isValid && (
+				<label className="text-field__error-message error-message">
+					Введите корректное значение
+				</label>
+			)}
+		</div>
+	);
+};
+
+export const LanguageField = ({ validate, isValid }) => {
+	const placeholderValue = "Язык";
+	const [value, setValue] = useState("");
+	// const handleChange = (event) => {
+	// 	setValue(event.target.value);
+	// 	validate(event.target.value, "language");
+	// };
+    const hideOrShow=()=>{
+        if(document.getElementsByClassName("text-field__choices")[0].offsetParent === null)
+            document.getElementsByClassName("text-field__choices")[0].style.display = 'block';
+        else
+            document.getElementsByClassName("text-field__choices")[0].style.display = 'none';
+    }
+    const makeChoise=(event)=>{
+        setValue(event.target.value);
+        hideOrShow();
+		validate(event.target.value, "language");
+    }
+	return (
+		<div className="form__text-field">
+			<label className="text-field__label">Язык</label>
+			<input
+				placeholder={placeholderValue}
+				value={value}
+                onClick={hideOrShow}
+                onBlur={hideOrShow}
+                readOnly={true}
+			/>
+            <div className="text-field__choices" >
+                <input className="choices__choice" value="Русский" readOnly={true} onClick={makeChoise}/>
+                <input className="choices__choice" value="Английский" readOnly={true} onClick={makeChoise}/>
+                <input className="choices__choice" value="Китайский" readOnly={true} onClick={makeChoise}/>
+                <input className="choices__choice" value="Испанский" readOnly={true} onClick={makeChoise}/>
+            </div>
+			{!isValid && (
+				<label className="text-field__error-message error-message">
+					Введите корректное значение
+				</label>
+			)}
+		</div>
+	);
+};
+
+export const CheckField = ({ validate, isValid }) => {
+	const [value, setValue] = useState(false);
+	const handleChange = () => {
+		setValue(!value);
+		validate(!value, "check");
+	};
+	return (
+		<div className="check-field">
+			<input type="checkbox" checked={value} onChange={handleChange} />
+			Принимаю <a href="#">условия</a> использования
+            <br/>
+			{!isValid && (
+				<label className="check-field__error-message error-message">
+					Для продолжения примите условия использования
+				</label>
+			)}
+		</div>
+	);
+};
